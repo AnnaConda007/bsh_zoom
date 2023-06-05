b742f1220987189cdabec20cc2558451eb8e453f
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-05 21:48:21 +0400

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,34 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:prettier/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:stylelint/recommended',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+		'react/react-in-jsx-scope': 'off',
+		'jsx-a11y/accessible-emoji': 'off',
+		'react/prop-types': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
+		'jsx-a11y/anchor-is-valid': [
+			'error',
+			{
+				components: ['Link'],
+				specialLink: ['hrefLeft', 'hrefRight'],
+				aspects: ['invalidHref', 'preferButton'],
+			},
+		],
+	},
+};
