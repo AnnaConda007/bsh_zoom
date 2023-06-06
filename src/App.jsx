b60a940d899923af2d7d1911bf3a9cead0bdd907
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-06 15:50:54 +0400

    состояния инпутов фиксируются в объекте

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Button, Form, FormControl } from 'react-bootstrap';
+import { useState } from 'react';
 function App() {
-	return <div>///</div>;
+	const formDefault = {
+		email: '+',
+		password: '/',
+	};
+	const [formValue, setFormValue] = useState(formDefault);
+	const { email } = formValue;
+	const { password } = formValue;
+	const changeInput = (e) => {
+		const { name, value } = e.target;
+		setFormValue({ ...formValue, [name]: value });
+		console.log(formValue);
+	};
+
+	return (
+		<div>
+			<Form>
+				<FormControl onChange={changeInput} value={email} name='email'></FormControl>
+				<FormControl onChange={changeInput} value={password} name='password'></FormControl>
+				<Button variant='primary'>отправить</Button>
+			</Form>
+		</div>
+	);
 }
 
 export default App;
