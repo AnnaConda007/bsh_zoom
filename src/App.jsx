7fb40976b0e86d1debdc665c4074a061eb534e11
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-06 20:30:48 +0400

    сборка работает без ошибок

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Authorization from '../routes/authorization/authorization.component';
 function App() {
-	return (
-		<Routes>
-			<Route path='/' element />
-			<Route path='authorization' element={<Authorization />}></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path='/' element />
+      <Route path='authorization' element={<Authorization />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import './index.styles.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
-import './index.styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
