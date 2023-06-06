c01752d213b93f774e92261c492ae270223e35f8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-06 16:22:11 +0400

    работает запрос к дб с логин/пароль

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Button, Form, FormControl } from 'react-bootstrap';
 import { useState } from 'react';
 function App() {
 	const formDefault = {
-		email: '+',
-		password: '/',
+		email: '',
+		password: '',
 	};
 	const [formValue, setFormValue] = useState(formDefault);
 	const { email } = formValue;
@@ -11,15 +11,29 @@ function App() {
 	const changeInput = (e) => {
 		const { name, value } = e.target;
 		setFormValue({ ...formValue, [name]: value });
-		console.log(formValue);
+	};
+
+	const authorizationResponse = async () => {
+		const apiKey = 'AIzaSyB4c4RDOCAaTXro1HTbNH857drwGWX-K20';
+		const dbUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+		const response = await fetch(`${dbUrl}${apiKey}`, {
+			method: 'POST',
+			body: JSON.stringify(formValue),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+		console.log(response);
 	};
 
 	return (
 		<div>
-			<Form>
+			<Form onSubmit={authorizationResponse}>
 				<FormControl onChange={changeInput} value={email} name='email'></FormControl>
 				<FormControl onChange={changeInput} value={password} name='password'></FormControl>
-				<Button variant='primary'>отправить</Button>
+				<Button variant='primary' onClick={authorizationResponse}>
+					отправить
+				</Button>
 			</Form>
 		</div>
 	);
