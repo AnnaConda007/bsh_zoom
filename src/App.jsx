d16818c613e2908230349f2e434896db42f6040c
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-06 16:50:26 +0400

    сообщение о неверном логине/пароле

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Button, Form, FormControl } from 'react-bootstrap';
 import { useState } from 'react';
+import { errorMonitor } from 'events';
 function App() {
 	const formDefault = {
-		email: '',
-		password: '',
+		email: 'user@mail.ru',
+		password: '111111',
 	};
 	const [formValue, setFormValue] = useState(formDefault);
 	const { email } = formValue;
@@ -14,16 +15,34 @@ function App() {
 	};
 
 	const authorizationResponse = async () => {
-		const apiKey = 'AIzaSyB4c4RDOCAaTXro1HTbNH857drwGWX-K20';
-		const dbUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
-		const response = await fetch(`${dbUrl}${apiKey}`, {
-			method: 'POST',
-			body: JSON.stringify(formValue),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
-		console.log(response);
+		try {
+			const apiKey = 'AIzaSyB4c4RDOCAaTXro1HTbNH857drwGWX-K20';
+			const dbUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+			const response = await fetch(`${dbUrl}${apiKey}`, {
+				method: 'POST',
+				body: JSON.stringify(formValue),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			if (!response.ok) {
+				const data = await response.json();
+				const errorMessage = data.error.message;
+				console.log(errorMessage);
+				switch (errorMessage) {
+					case 'INVALID_PASSWORD':
+						alert('Неверный пароль');
+						break;
+					case 'EMAIL_NOT_FOUND':
+						alert('Неверный email');
+						break;
+					default:
+						console.log(error);
+				}
+			}
+		} catch (error) {
+			throw new Error(error);
+		}
 	};
 
 	return (
