eca3ce2e8ca437c53454132311a2714331154c14
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-08 00:23:00 +0400

    moment =>luxon

--- utils/getTime.utils.jsx
+++ utils/getTime.utils.jsx
@@ -1,5 +1,4 @@
-import moment from 'moment';
-//import { DateTime } from 'luxon';
+import { DateTime } from 'luxon';
 
 import {} from 'luxon';
 const getfixAuthorizationTime = async () => {
@@ -7,18 +6,13 @@ const getfixAuthorizationTime = async () => {
 	try {
 		const response = await fetch('http://worldtimeapi.org/api/timezone/Russia/Moscow');
 		const data = await response.json();
-		const dateTime = moment(data.datetime);
-		const timestamp = Math.floor(dateTime.valueOf() / 1000);
-		currentTime = timestamp || Math.floor(Date.now() / 1000);
+		const dateTime = DateTime.fromISO(data.datetime);
+		const timestamp = Math.floor(dateTime.toSeconds());
+		currentTime = timestamp || Math.floor(DateTime.local().toSeconds());
+		console.log(currentTime);
 	} catch (error) {
 		console.error('Ошибка при обращении к API,опредяляющему точное время :', error);
 	}
 	return currentTime;
 };
 export default getfixAuthorizationTime;
-
-/*
-    const dateTime = DateTime.fromISO(data.datetime);
-    const timestamp = Math.floor(dateTime.toSeconds());
-    currentTime = timestamp || Math.floor(DateTime.local().toSeconds());
-*/
