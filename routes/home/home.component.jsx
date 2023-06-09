d2fa59c7a42fc602bf37f48662cd27addc1aab27
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-09 23:58:23 +0400

    ошибка при переходе из home к авторизации

--- routes/home/home.component.jsx
+++ routes/home/home.component.jsx
@@ -1,15 +1,23 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import getfixAuthorizationTime from '../../utils/getTime.utils';
 const Home = () => {
-  const navigate = useNavigate();
- 	const autosaveTime = 604800;
-  useEffect(() => {
-    const isExpired = 1;
-    if (!isExpired || isExpired >= autosaveTime) {
-    navigate("Authorization");
-  }
-}, []);
+	const navigate = useNavigate();
+	const autosaveTime = 30; //6048000
+	const authorizationTime = parseInt(localStorage.getItem('authorizationTime'));
+	const [currentTime, setCurrentTime] = useState(null);
+	useEffect(() => {
+		const fetchCurrentTime = async () => {
+			const time = await getfixAuthorizationTime();
+			setCurrentTime(time);
+		};
+		fetchCurrentTime();
+	}, []);
 
-return <p>календарь</p>;
+	const isExpired = authorizationTime + autosaveTime;
+	if (!authorizationTime || currentTime > isExpired) {
+		navigate('authorization');
+	}
+	return <p>календарь</p>;
 };
 export default Home;
