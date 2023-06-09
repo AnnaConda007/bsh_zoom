1d02110518d73b0432a9a16b05c27e14a6455152
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-10 00:07:52 +0400

    нет ошибки при переходе из home  к авторизации

--- routes/home/home.component.jsx
+++ routes/home/home.component.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import getfixAuthorizationTime from '../../utils/getTime.utils';
 const Home = () => {
 	const navigate = useNavigate();
-	const autosaveTime = 30; //6048000
+	const autosaveTime = 5; //6048000
 	const authorizationTime = parseInt(localStorage.getItem('authorizationTime'));
 	const [currentTime, setCurrentTime] = useState(null);
 	useEffect(() => {
@@ -13,11 +13,13 @@ const Home = () => {
 		};
 		fetchCurrentTime();
 	}, []);
+	useEffect(() => {
+		const isExpired = authorizationTime + autosaveTime;
+		if (!authorizationTime || currentTime > isExpired) {
+			navigate('authorization');
+		}
+	}, [currentTime]);
 
-	const isExpired = authorizationTime + autosaveTime;
-	if (!authorizationTime || currentTime > isExpired) {
-		navigate('authorization');
-	}
 	return <p>календарь</p>;
 };
 export default Home;
