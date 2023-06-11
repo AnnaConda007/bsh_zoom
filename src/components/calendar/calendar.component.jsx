7dbb76ec41aef364f31846906f5f168896a1e70e
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-11 21:33:38 +0400

    DatePicker не отображается

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -1,4 +1,11 @@
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'; // установлен как отдельный пакет, не mui
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'; // установлен вместе с  @mui x-date-pickers
 const Calendar = () => {
-	return <>календарь ! </>;
+	return (
+		<>
+			<DatePicker />
+			<p>текст, пропадающий при добавлении DatePicker </p>
+		</>
+	);
 };
 export default Calendar;
