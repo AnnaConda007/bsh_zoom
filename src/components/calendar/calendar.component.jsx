63693f922df943625baaf5af3dff38ae2a70da6d
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-12 15:21:26 +0400

    отображается стандартный компонент

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 const Calendar = () => {
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<DemoContainer components={['DatePicker']}>
-				<DatePicker label='Basic date picker' />
-			</DemoContainer>
+			<DateCalendar />
 		</LocalizationProvider>
 	);
 };
