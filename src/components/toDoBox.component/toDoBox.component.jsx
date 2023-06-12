a58fea82e12eb08f7ca9ed4904bce9e52fc49aba
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-12 19:23:21 +0400

    добавление новых тасков

--- src/components/toDoBox.component/toDoBox.component.jsx
+++ src/components/toDoBox.component/toDoBox.component.jsx
@@ -2,29 +2,56 @@ import { TextField, FormControl } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useState } from 'react';
 const ToDoBox = () => {
+	const [addTaskInput, setAddTaskInput] = useState('');
+	let [tasks, setTasks] = useState([]);
+
+	const handleСhangeInput = (e) => {
+		setAddTaskInput(e.target.value);
+	};
+	const handleAddTask = () => {
+		if (addTaskInput === '') return;
+		setTasks([...tasks, addTaskInput]);
+		setAddTaskInput('');
+	};
+	const handleEnterDown = (event) => {
+		if (event.key === 'Enter') {
+			handleAddTask();
+		}
+	};
+
 	return (
 		<>
 			<div className='to-do-wrap'>
 				<div className='to-do-wrap__add'>
 					<TextField
+						onKeyDown={handleEnterDown}
+						onChange={handleСhangeInput}
+						value={addTaskInput}
 						InputProps={{
-							endAdornment: <AddCircleOutlineIcon />,
+							endAdornment: <AddCircleOutlineIcon onClick={handleAddTask} />,
 						}}
 					/>
 				</div>
 				<div className='to-do-wrap__tasks'>
 					<FormControl>
-						<TextField
-							InputProps={{
-								endAdornment: (
-									<div>
-										<DeleteForeverIcon />
-										<ModeEditIcon />
-									</div>
-								),
-							}}
-						/>
+						{tasks.map((task, i) => {
+							return (
+								<TextField
+									key={i}
+									value={task}
+									InputProps={{
+										endAdornment: (
+											<div>
+												<DeleteForeverIcon />
+												<ModeEditIcon />
+											</div>
+										),
+									}}
+								/>
+							);
+						})}
 					</FormControl>
 				</div>
 			</div>
