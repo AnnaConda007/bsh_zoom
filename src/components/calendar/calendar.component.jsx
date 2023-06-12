7a6d3f641470102a82c6382e5b2ad295161e4cfd
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-12 17:17:51 +0400

    модальное окно при клике на дату

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -1,13 +1,43 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { Modal, Box, Typography } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 const Calendar = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleDateChange = (date) => {
+		console.log(date);
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<DateCalendar />
-		</LocalizationProvider>
+		<>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby='modal-modal-title'
+				aria-describedby='modal-modal-description'
+			>
+				<Box sx={{ color: 'red' }}>
+					Text in a modal
+				</Box>
+			</Modal>
+
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DateCalendar
+					onChange={(data) => {
+						handleDateChange(data);
+					}}
+				/>
+			</LocalizationProvider>
+		</>
 	);
 };
 
