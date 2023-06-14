c82cfcfe1a94269a69f92791c6b4a95dce37068c
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-14 18:04:06 +0400

    часть стилей переопредляется

--- src/main.jsx
+++ src/main.jsx
@@ -9,20 +9,39 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#9a1ac1',
-    },
-  },
-  
+	palette: {
+		primary: {
+			main: '#9a1ac1',
+		},
+	},
+	components: {
+		MuiDateCalendar: {
+			styleOverrides: {
+				root: {
+					width: '100%',
+					height: '100%',
+				},
+				viewTransitionContainer: {
+					width: '100%',
+					height: '100%',
+				},
+				header: {
+					justifyContent: 'space-evenly',
+				},
+				weekContainer: { 
+					justifyContent: 'space-evenly',
+				},
+			},
+		},
+	},
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
