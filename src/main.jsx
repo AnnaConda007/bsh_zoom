37d80a570999ff7ce7cdef0afd9c33d2022cabe8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-15 11:26:50 +0400

    убраны селекторы с обфусцированными классами

--- src/main.jsx
+++ src/main.jsx
@@ -18,32 +18,56 @@ const theme = createTheme({
 		MuiDateCalendar: {
 			styleOverrides: {
 				root: {
+					//root
 					width: '100%',
 					height: '100%',
 					maxHeight: '100%',
+					overflow: 'auto', // возможно
 				},
 				viewTransitionContainer: {
+					// главная обертка для календаря
 					width: '100%',
 					height: '100%',
 					'& > div': {
+						//опасити
 						height: '100%',
 						'& > div': {
+							// грид
 							height: '100%',
+							'& > div:first-child': {
+								// название дней недели
+								justifyContent: 'space-evenly',
+								backgroundColor: 'red',
+							},
+							'& > div:last-child': {
+								// контейнер для календаря
+								backgroundColor: 'blue',
+								height: '100%',
+								'& > div': {
+									height: '100%',
+									backgroundColor: 'red',
+									'& > div': {
+										backgroundColor: 'blue',
+
+										justifyContent: 'space-evenly',
+									},
+								},
+							},
 						},
 					},
-				},
+				} /*
 				'& .css-i5q14k-MuiDayCalendar-header': {
 					justifyContent: 'space-evenly',
 				},
 				'& .css-flbe84-MuiDayCalendar-weekContainer': {
 					justifyContent: 'space-evenly',
-				},
+				},*/,
 				'& .css-v20sp3-MuiButtonBase-root-MuiPickersDay-root': {
 					width: '70px',
 					height: '70px',
 					fontWeight: '400',
 					backgroundColor: 'red',
-				},
+				} /*
 				'css-i5q14k-MuiDayCalendar-header': {
 					justifyContent: 'space-evenly',
 				},
@@ -55,7 +79,7 @@ const theme = createTheme({
 					height: '70px',
 					fontWeight: '400px',
 					backgroundColor: 'red',
-				},
+				},*/,
 			},
 		},
 	},
