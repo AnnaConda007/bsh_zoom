d54ec3885be60812f360ad0dd2716c6867a131e5
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-15 16:46:59 +0400

    тема для app  вынесена в отдельный файл

--- src/main.jsx
+++ src/main.jsx
@@ -1,80 +1,17 @@
 import './index.styles.scss';
-
 import { ThemeProvider } from '@mui/material/styles';
-import { createTheme } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
+import theme from './theme';
 import App from './App.jsx';
-
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#9a1ac1',
-		},
-	},
-	components: {
-		MuiDateCalendar: {
-			styleOverrides: {
-				root: {
-					//root
-					width: '100%',
-					height: '100%',
-					maxHeight: '700px',
-					overflow: 'auto',
-				},
-				viewTransitionContainer: {
-					// главная обертка для календаря
-					width: '100%',
-					height: '100%',
-					'& > div': {
-						// общая бертка для названия дней недели и календаря
-						height: '100%',
-						'& > div': {
-							// грид-
-							height: '100%',
-							'& > div:first-of-type': {
-								// название дней недели
-								justifyContent: 'space-evenly',
-								backgroundColor: '#dbdbeb',
-								'& > span': {
-									fontSize: '1rem',
-								},
-							},
-							'& > div:last-child': {
-								// контейнер для дат
-								height: '80%',
-								marginTop: '30px',
-								backgroundColor: ' ', //!!!!заменить на primary
-
-								'& > div': {
-									// промежуточный контейнер
-									height: '100%',
-									display: 'flex',
-									flexDirection: 'column',
-									justifyContent: 'space-between',
-									'& > div': {
-										justifyContent: 'space-evenly', // контейнер для дней недели
-										'& > button': {
-											fontWeight: '800',
-											fontSize: '1rem',
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	},
-});
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<ThemeProvider theme={theme}>
+				<CssBaseline />
 				<App />
 			</ThemeProvider>
 		</BrowserRouter>
