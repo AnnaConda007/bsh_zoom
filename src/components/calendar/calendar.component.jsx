fec88e9883c6cc9ad6740bce273460488d447246
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-16 12:12:22 +0400

    удалено day-holiday

--- src/components/calendar/calendar.component.jsx
+++ src/components/calendar/calendar.component.jsx
@@ -11,7 +11,6 @@ import 'dayjs/locale/ru';
 dayjs.locale('ru');
 import theme from './theme';
 import { ThemeProvider } from '@mui/material/styles';
-//import { Holidays } from 'date-holidays';   
 
 const Calendar = () => {
 	const [open, setOpen] = useState(false);
