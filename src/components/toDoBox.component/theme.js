76da8572e43a0d7f5549af8e21c2d528d798f6d8
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-17 19:10:54 +0400

    убрано дублирование темы

--- src/components/toDoBox.component/theme.js
+++ src/components/toDoBox.component/theme.js
@@ -1,11 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-		palette: {
-			primary: {
-				main: '#dbdbeb',
-			},
-		},
 		components: {
 			MuiTextField: {
 				styleOverrides: {
