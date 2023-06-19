65fd609c5f7547ee4bac624c0abdb82beafa9a0f
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-19 12:41:20 +0400

    задачи удаляются, редактируются

--- utils/updateTask.jsx
+++ utils/updateTask.jsx
@@ -1,23 +1,27 @@
 const dbUrl = 'https://test-f176b-default-rtdb.firebaseio.com/.json';
+let taskForDate;
+let tasks;
+let date;
 export const pullTask = async (formattedDate) => {
 	// сейчас нигде не вызывается
 	const res = await fetch(dbUrl);
 	const resJson = await res.json();
-	const tasks = resJson ? resJson : [];
-
-	const date = formattedDate;
-//	console.log(formattedDate);
-	const taskForDate = tasks[date] || [];
+	tasks = resJson ? resJson : [];
+	date = formattedDate;
+	taskForDate = tasks[date] || [];
 	//	setTasks(taskForDate);
-//	console.log(taskForDate);
 	return taskForDate;
 };
+
 export const pushTasks = async (updatedTasks) => {
+	tasks[date] = updatedTasks;
 	await fetch(dbUrl, {
 		method: 'PUT',
-		body: JSON.stringify(updatedTasks),
+		body: JSON.stringify(tasks),
 		headers: {
 			'Content-Type': 'application/json',
 		},
 	});
 };
+
+
