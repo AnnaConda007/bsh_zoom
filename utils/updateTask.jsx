00ae1472fac25433ef7a29d6e93460fe0355c787
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-21 16:22:24 +0400

    нет ошибки при пустой бд

--- utils/updateTask.jsx
+++ utils/updateTask.jsx
@@ -6,13 +6,14 @@ let selectedDate;
 export const pullTask = async (formattedDate) => {
 	const res = await fetch(dataBaseUrl);
 	resJson = await res.json();
-	selectedDate =  formattedDate  
+	resJson = resJson ? resJson : {};
+	selectedDate = formattedDate;
 	taskForDate = resJson[selectedDate] || []; // маасив объектов для даты
 	console.log(taskForDate);
 	return taskForDate;
 };
 
- export const getDayTask = async () => {
+export const getDayTask = async () => {
 	const res = await fetch(dataBaseUrl);
 	resJson = await res.json();
 	const datesArray = resJson ? Object.keys(resJson) : [];
