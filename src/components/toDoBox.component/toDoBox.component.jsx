7294816d94b48033372816042276f20e5d85a68c
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-22 21:14:27 +0400

    очищен toDoBox

--- src/components/toDoBox.component/toDoBox.component.jsx
+++ src/components/toDoBox.component/toDoBox.component.jsx
@@ -1,55 +1,10 @@
 import { useState } from 'react';
 import './toDoBox.styles.scss';
-import { pushTasks } from '../../../utils/updateTask';
-import dayjs from 'dayjs';
 import AddNewTask from './addNewTask/AddNewTask';
 import AddedTasks from './addedTasks/addedTasks';
 
 const ToDoBox = ({ tasksForSelectedDate, dates, setDates, activeDate }) => {
 	const [pulledTasks, setPulledTasks] = useState(tasksForSelectedDate);
-	const [isEditingIndex, setisEditingIndex] = useState(null);
-	const [editingValue, setEditingValue] = useState('');
-
-	const upDateTimeForAddedTask = (time, index, timeKey) => {
-		const updatedTasks = [...pulledTasks];
-		updatedTasks[index][timeKey] = time.toISOString();
-		pushTasks(updatedTasks);
-		setPulledTasks(updatedTasks);
-	};
-
-	const handleEditBtn = (index) => {
-		setisEditingIndex(index);
-		setEditingValue(pulledTasks[index].taskValue);
-	};
-
-	const handleTaskInputChange = (newValue) => {
-		setEditingValue(newValue);
-	};
-
-	const handleSaveEdit = (index) => {
-		const updatedTasks = [...pulledTasks];
-		updatedTasks[index].taskValue = editingValue;
-		pushTasks(updatedTasks);
-		setPulledTasks(updatedTasks);
-		setisEditingIndex(null);
-	};
-
-	const handleCancelEdit = () => {
-		setisEditingIndex(null);
-	};
-
-	const handleDeleteBtn = (index) => {
-		const updatedTasks = pulledTasks.filter((_, i) => i !== index);
-		pushTasks(updatedTasks);
-		setPulledTasks(updatedTasks);
-		if (index === isEditingIndex) {
-			setisEditingIndex(null);
-		}
-		const formattedDate = dayjs(activeDate.day.$d).format('DD-MM-YYYY');
-		if (dates.includes(formattedDate)) {
-			setDates((prevDates) => prevDates.filter((date) => date !== formattedDate));
-		}
-	};
 
 	return (
 		<>
