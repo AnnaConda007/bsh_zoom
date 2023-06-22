69a3bc10be89b580e73f9eda3163d7b65f3cb546
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-22 23:00:18 +0400

    стили для блока новых тасков

--- src/components/toDoBox.component/addNewTask/addNewTask.jsx
+++ src/components/toDoBox.component/addNewTask/addNewTask.jsx
@@ -6,7 +6,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import dayjs from 'dayjs';
-
+import styles from './addNewTask.module.scss';
 const AddNewTask = ({ dates, setDates, activeDate, pulledTasks, setPulledTasks }) => {
 	const defaultTask = { taskValue: '', timeStart: '', timeEnd: '' };
 	const [newTaskObj, setNewTaskObj] = useState(defaultTask);
@@ -40,9 +40,10 @@ const AddNewTask = ({ dates, setDates, activeDate, pulledTasks, setPulledTasks }
 
 	return (
 		<>
-			<div className='planner__textField-wrap planner__textField-wrap--add'>
+			<div className={styles.newTask}>
 				<TextField
-					className='planner__textField'
+					sx={{ border: '1px solid', borderRadius: '5px' }}
+					className={styles.newTask__textField}
 					multiline={true}
 					onChange={(e) => {
 						fullnessValueForNewTask(e.target.value);
@@ -50,12 +51,11 @@ const AddNewTask = ({ dates, setDates, activeDate, pulledTasks, setPulledTasks }
 					value={newTaskObj.taskValue}
 					InputProps={{
 						endAdornment: (
-							<div className='add__time'>
+							<div className={styles.newTask__time}>
 								<LocalizationProvider dateAdapter={AdapterDayjs}>
 									<TimePicker
 										label='начало'
 										ampm={false}
-										className='add__start-time --timePicker'
 										value={newTaskObj.timeStart}
 										onChange={(time) => {
 											fullnessTimeForNewTask(time, 'timeStart');
@@ -65,7 +65,6 @@ const AddNewTask = ({ dates, setDates, activeDate, pulledTasks, setPulledTasks }
 										label='конец'
 										value={newTaskObj.timeEnd}
 										ampm={false}
-										className='add__end-time --timePicker'
 										onChange={(time) => {
 											fullnessTimeForNewTask(time, 'timeEnd');
 										}}
