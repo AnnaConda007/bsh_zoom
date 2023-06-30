b75c9a96600a75b473f91977bcbdfd5b928ec1ce
Anna Hrulckova <annahrulkova@yandex.ru>
2023-06-30 17:23:43 +0400

    добавлена страница ZoomRedirect

---
+++ routes/zoom-redirect/zoom-redirect.component.jsx
@@ -0,0 +1,4 @@
+const ZoomRedirect = () => {
+	return <h1>zoom zoom</h1>;
+};
+export default ZoomRedirect;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import { ActiveDateProvider } from './contexts/activeDateContext';
- import Authorization from '../routes/authorization/authorization.component';
+import Authorization from '../routes/authorization/authorization.component';
 import Home from '../routes/home/home.component';
+import ZoomRedirect from '../routes/zoom-redirect/zoom-redirect.component';
 import { Providers } from './contexts/providers';
- function App() {
+function App() {
 	return (
 		<Providers>
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='authorization' element={<Authorization />} />
+				<Route path='zoom' element={<ZoomRedirect />} />
 			</Routes>
 		</Providers>
 	);
