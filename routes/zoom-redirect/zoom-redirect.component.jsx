189211c7c9a0937d3e6630354055070601982190
Anna Hrulckova <annahrulkova@yandex.ru>
2023-07-08 16:36:01 +0400

    переписан запрос

--- routes/zoom-redirect/zoom-redirect.component.jsx
+++ routes/zoom-redirect/zoom-redirect.component.jsx
@@ -1,14 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { pullZoomData } from '../../utils/updateTask';
 import { getUrlMeeting } from '../../utils/updateTask';
 import { calculateMinuteDifference } from '../../utils/dateForZoom.utils';
 const ZoomRedirect = () => {
-	const conferenceTopic = localStorage.getItem('conferenceTopic') || null;
-	const timeStart = localStorage.getItem('timeStart') || null;
-	const timeEnd = localStorage.getItem('timeEnd') || null;
-	const conferenceDuration = calculateMinuteDifference(timeStart, timeEnd);
-	console.log('renderTimeSt', timeStart);
+	const [accessToken, setAccessToken] = useState('');
+
 	useEffect(() => {
 		const getToken = async () => {
 			const urlParams = new URLSearchParams(window.location.search);
@@ -18,18 +15,37 @@ const ZoomRedirect = () => {
 					.get('http://localhost:3000/exchangeCode', {
 						params: {
 							code: authorizationCode,
-							conferenceTopic: conferenceTopic,
-							timeStart: timeStart,
-							conferenceDuration: conferenceDuration,
 						},
 					})
 					.then((response) => {
-						console.log(response.data);
+						setAccessToken(response.data.access_token);
 					});
 			}
 		};
 		getToken();
 	}, []);
+
+	useEffect(() => {
+		if (accessToken) {
+			const conferenceTopic = localStorage.getItem('conferenceTopic') || null;
+			const timeStart = localStorage.getItem('timeStart') || null;
+			const timeEnd = localStorage.getItem('timeEnd') || null;
+			const conferenceDuration = calculateMinuteDifference(timeStart, timeEnd);
+			const token = accessToken;
+			axios
+				.get('http://localhost:3000/newConference', {
+					params: {
+						conferenceTopic: conferenceTopic,
+						timeStart: timeStart,
+						conferenceDuration: conferenceDuration,
+						token: token,
+					},
+				})
+				.then((response) => {
+					console.log(response.data.meeting);
+				});
+		}
+	}, [accessToken]);
 
 	return <h1>Перенаправление...</h1>;
 };
--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const port = 3000;
 app.use(cors());
 
 app.get('/exchangeCode', async (req, res) => { 
-    const authorizationCode = req.query.code;
-    const conferenceTopic =req.query.conferenceTopic;
-	const timeStart= req.query.timeStart;
-	const conferenceDuration=  req.query.conferenceDuration;
-    console.log("timeStart", )
+    const authorizationCode = req.query.code; 
   try {
         const response = await axios.post('https://zoom.us/oauth/token', null, {
             params: {
@@ -26,10 +22,20 @@ app.get('/exchangeCode', async (req, res) => {
             },
         });
         const accessToken = response.data.access_token;
-//  res.send({ access_token: accessToken });
+  res.send({ access_token: accessToken });
 
+  } catch (error) {
+    console.error('Error exchanging code for token:', error);
+    res.status(500).send('Error exchanging code for token');
+  }
+});
 
- try {
+app.get('/newConference', async (req, res) => {
+    const accessToken =req.query.token;
+    const conferenceTopic =req.query.conferenceTopic;
+	const timeStart= req.query.timeStart;
+	const conferenceDuration=  req.query.conferenceDuration;
+     try {
  const meetingResponse = await axios.post(
         'https://api.zoom.us/v2/users/me/meetings',
         {
@@ -55,15 +61,42 @@ app.get('/exchangeCode', async (req, res) => {
       console.log('Error creating Zoom meeting:', error.response.data);
       res.status(500).send('Error creating Zoom meeting');
     }
+ })
 
 
 
-  } catch (error) {
-    console.error('Error exchanging code for token:', error);
-    res.status(500).send('Error exchanging code for token');
-  }
-});
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+
+
+/*
+ try {
+ const meetingResponse = await axios.post(
+        'https://api.zoom.us/v2/users/me/meetings',
+        {
+          topic:conferenceTopic,
+          pre_schedule:true,
+          type: 2,  
+          start_time:timeStart,  
+          duration: conferenceDuration,  
+          settings: {
+            host_video: true,
+            participant_video: true,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      ); 
+ res.send({  meeting: meetingResponse.data });
+    } catch (error) {
+      console.log('Error creating Zoom meeting:', error.response.data);
+      res.status(500).send('Error creating Zoom meeting');
+    } */
