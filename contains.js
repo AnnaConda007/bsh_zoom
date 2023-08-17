f908e0360da9d65a6cf73be9670a4307be923e19
Anna Hrulckova <annahrulkova@yandex.ru>
2023-08-17 22:27:31 +0400

    для нетлифай

--- contains.js
+++ contains.js
@@ -1,10 +1,9 @@
-export const clientId = 'wYILEd3tQnCCk4CE6Jihxg'
-export const clientSecret = 'nRPLBGGecg3O2VaUre8c6C7xPvJTboaZ'
-export const zoomVebHookSecretToken = 'cLdi2VzFS3GIyTD5qYR3hQ'
-export const homeUrL = 'http://localhost:5173'
+export const clientId = 'mkQg8GOmTSem7YxdUujxAA'
+export const clientSecret = 'YQElNfVwH7Wo7zhvF0WV5HmIZw6jqDr0'
+export const homeUrL = 'https://snazzy-muffin-3a5123.netlify.app/'
 export const redirectHomeUrL = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(homeUrL)}`
-export const serverUrl = 'https://localhost:3000'
-export const dataBaseUrl = 'https://test-f176b-default-rtdb.firebaseio.com/timeArr.json'
+export const serverUrl = 'https://servzoom.onrender.com'
+export const dataBaseUrl = 'https://bsh-app-3e342-default-rtdb.firebaseio.com/timeArr.json'
 export const autosaveTime = 604800000 // неделя
 export const disabledMeeting = 'Редактирование чужих задач невозможно'
 export const errorMessageForCompareErrorTime = 'Время начала конференции не может быть позже времени её окончания или совпадать с ней'
