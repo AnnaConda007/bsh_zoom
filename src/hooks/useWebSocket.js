60ecc735017fbca2e5731cc847de1ae34bdf0857
Anna Hrulckova <annahrulkova@yandex.ru>
2023-08-21 13:41:50 +0400

    wss

--- src/hooks/useWebSocket.js
+++ src/hooks/useWebSocket.js
@@ -4,19 +4,19 @@ import { DatesContext } from '../contexts/dates.context'
 const useWebSocket = () => {
   const { setUpDateTaggedDateNeed } = useContext(DatesContext)
   useEffect(() => {
-    const ws = new WebSocket(vebSocketUrl)
-    ws.onopen = () => {
+    const wss = new WebSocket(vebSocketUrl)
+    wss.onopen = () => {
       console.log('WebSocket соединение установлено.')
     }
-    ws.onmessage = (message) => {
+    wss.onmessage = (message) => {
       console.log('message', message)
       setUpDateTaggedDateNeed(true)
     }
-    ws.onerror = (error) => {
+    wss.onerror = (error) => {
       console.error('Ошибка webSocket:', error)
     }
     return () => {
-      ws.close()
+      wss.close()
     }
   }, [])
 }
